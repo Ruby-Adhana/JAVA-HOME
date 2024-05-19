65094ebf9b53eebb65f201cb047cb6df61beb501
Ruby-Adhana <145450062+Ruby-Adhana@users.noreply.github.com>
2024-05-19 22:08:55 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -2,12 +2,10 @@ public class Array {
     public static void main(String[] args) {
         System.out.println("Arrays");
 
-        int marks[] =new int[5];
+        int marks[] =new int[3];
         marks[0]=001;
         marks[1]=002;
         marks[2]=003;
-        marks[1]=004;
-        marks[2]=005;
         System.out.println(marks[1]);
         System.out.println("Print by loop");
         for(int i=0;i< marks.length;i++){
