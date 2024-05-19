a5aa5b0a173dedb7df58a43903164e2c81921f2c
Ruby-Adhana <145450062+Ruby-Adhana@users.noreply.github.com>
2024-05-19 22:04:13 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -3,13 +3,16 @@ public class Array {
         System.out.println("Arrays");
 
         int marks[] =new int[3];
-        marks[0]=125;
-        marks[1]=1023;
-        marks[2]=189;
+        marks[0]=001;
+        marks[1]=002;
+        marks[2]=003;
+        marks[1]=004;
+        marks[2]=005;
         System.out.println(marks[1]);
         System.out.println("Print by loop");
         for(int i=0;i< marks.length;i++){
             System.out.println(marks[i]);
+            System.out.println[i];
         }
     }
 }
